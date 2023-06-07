c56f51a8b9498f082a67ea69155f25161a5875b3
Bùi Xuân Hoàng Oanh <62228385+HoangOanh0711@users.noreply.github.com>
2023-06-08 00:12:43 +0700

    update dictionary

--- lab3/Dictionary.Designer.cs
+++ lab3/Dictionary.Designer.cs
@@ -28,10 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dictionary));
             this.guna2CirclePictureBox1 = new Guna.UI2.WinForms.Guna2CirclePictureBox();
             this.guna2CirclePictureBox6 = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            this.guna2DataGridView1 = new Guna.UI2.WinForms.Guna2DataGridView();
+            this.guna2Button3 = new Guna.UI2.WinForms.Guna2Button();
+            this.btn_fruit = new Guna.UI2.WinForms.Guna2Button();
+            this.btn_animal = new Guna.UI2.WinForms.Guna2Button();
+            this.btn_color = new Guna.UI2.WinForms.Guna2Button();
+            this.btn_job = new Guna.UI2.WinForms.Guna2Button();
+            this.Column1 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.guna2CirclePictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.guna2CirclePictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2DataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // guna2CirclePictureBox1
@@ -62,21 +77,205 @@
             this.guna2CirclePictureBox6.TabStop = false;
             this.guna2CirclePictureBox6.Click += new System.EventHandler(this.guna2CirclePictureBox6_Click);
             // 
+            // guna2DataGridView1
+            // 
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.White;
+            this.guna2DataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.guna2DataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.guna2DataGridView1.ColumnHeadersHeight = 26;
+            this.guna2DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
+            this.guna2DataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.guna2DataGridView1.DefaultCellStyle = dataGridViewCellStyle4;
+            this.guna2DataGridView1.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.guna2DataGridView1.Location = new System.Drawing.Point(198, 78);
+            this.guna2DataGridView1.Name = "guna2DataGridView1";
+            this.guna2DataGridView1.RowHeadersVisible = false;
+            this.guna2DataGridView1.RowTemplate.DefaultCellStyle.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.guna2DataGridView1.RowTemplate.DefaultCellStyle.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2DataGridView1.RowTemplate.Height = 200;
+            this.guna2DataGridView1.Size = new System.Drawing.Size(887, 583);
+            this.guna2DataGridView1.TabIndex = 28;
+            this.guna2DataGridView1.ThemeStyle.AlternatingRowsStyle.BackColor = System.Drawing.Color.White;
+            this.guna2DataGridView1.ThemeStyle.AlternatingRowsStyle.Font = null;
+            this.guna2DataGridView1.ThemeStyle.AlternatingRowsStyle.ForeColor = System.Drawing.Color.Empty;
+            this.guna2DataGridView1.ThemeStyle.AlternatingRowsStyle.SelectionBackColor = System.Drawing.Color.Empty;
+            this.guna2DataGridView1.ThemeStyle.AlternatingRowsStyle.SelectionForeColor = System.Drawing.Color.Empty;
+            this.guna2DataGridView1.ThemeStyle.BackColor = System.Drawing.Color.White;
+            this.guna2DataGridView1.ThemeStyle.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.guna2DataGridView1.ThemeStyle.HeaderStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.guna2DataGridView1.ThemeStyle.HeaderStyle.BorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.guna2DataGridView1.ThemeStyle.HeaderStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2DataGridView1.ThemeStyle.HeaderStyle.ForeColor = System.Drawing.Color.White;
+            this.guna2DataGridView1.ThemeStyle.HeaderStyle.HeaightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
+            this.guna2DataGridView1.ThemeStyle.HeaderStyle.Height = 26;
+            this.guna2DataGridView1.ThemeStyle.ReadOnly = false;
+            this.guna2DataGridView1.ThemeStyle.RowsStyle.BackColor = System.Drawing.Color.White;
+            this.guna2DataGridView1.ThemeStyle.RowsStyle.BorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.guna2DataGridView1.ThemeStyle.RowsStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2DataGridView1.ThemeStyle.RowsStyle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            this.guna2DataGridView1.ThemeStyle.RowsStyle.Height = 200;
+            this.guna2DataGridView1.ThemeStyle.RowsStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.guna2DataGridView1.ThemeStyle.RowsStyle.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            // 
+            // guna2Button3
+            // 
+            this.guna2Button3.BorderRadius = 5;
+            this.guna2Button3.BorderThickness = 1;
+            this.guna2Button3.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button3.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button3.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button3.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button3.FillColor = System.Drawing.Color.White;
+            this.guna2Button3.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2Button3.ForeColor = System.Drawing.Color.Black;
+            this.guna2Button3.Location = new System.Drawing.Point(926, 12);
+            this.guna2Button3.Name = "guna2Button3";
+            this.guna2Button3.Size = new System.Drawing.Size(159, 49);
+            this.guna2Button3.TabIndex = 31;
+            this.guna2Button3.Text = "ADD";
+            this.guna2Button3.Click += new System.EventHandler(this.guna2Button3_Click);
+            // 
+            // btn_fruit
+            // 
+            this.btn_fruit.BackColor = System.Drawing.Color.Transparent;
+            this.btn_fruit.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(169)))), ((int)(((byte)(146)))));
+            this.btn_fruit.BorderThickness = 2;
+            this.btn_fruit.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btn_fruit.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btn_fruit.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btn_fruit.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btn_fruit.FillColor = System.Drawing.Color.Transparent;
+            this.btn_fruit.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_fruit.ForeColor = System.Drawing.Color.Black;
+            this.btn_fruit.Location = new System.Drawing.Point(12, 230);
+            this.btn_fruit.Name = "btn_fruit";
+            this.btn_fruit.Size = new System.Drawing.Size(180, 45);
+            this.btn_fruit.TabIndex = 49;
+            this.btn_fruit.Tag = "4";
+            this.btn_fruit.Text = "Fruits";
+            this.btn_fruit.Click += new System.EventHandler(this.btn_fruit_Click);
+            // 
+            // btn_animal
+            // 
+            this.btn_animal.BackColor = System.Drawing.Color.Transparent;
+            this.btn_animal.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(169)))), ((int)(((byte)(146)))));
+            this.btn_animal.BorderThickness = 2;
+            this.btn_animal.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btn_animal.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btn_animal.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btn_animal.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btn_animal.FillColor = System.Drawing.Color.Transparent;
+            this.btn_animal.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_animal.ForeColor = System.Drawing.Color.Black;
+            this.btn_animal.Location = new System.Drawing.Point(12, 179);
+            this.btn_animal.Name = "btn_animal";
+            this.btn_animal.Size = new System.Drawing.Size(180, 45);
+            this.btn_animal.TabIndex = 48;
+            this.btn_animal.Tag = "3";
+            this.btn_animal.Text = "Animals";
+            this.btn_animal.Click += new System.EventHandler(this.btn_animal_Click);
+            // 
+            // btn_color
+            // 
+            this.btn_color.BackColor = System.Drawing.Color.Transparent;
+            this.btn_color.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(169)))), ((int)(((byte)(146)))));
+            this.btn_color.BorderThickness = 2;
+            this.btn_color.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btn_color.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btn_color.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btn_color.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btn_color.FillColor = System.Drawing.Color.Transparent;
+            this.btn_color.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_color.ForeColor = System.Drawing.Color.Black;
+            this.btn_color.Location = new System.Drawing.Point(12, 128);
+            this.btn_color.Name = "btn_color";
+            this.btn_color.Size = new System.Drawing.Size(180, 45);
+            this.btn_color.TabIndex = 47;
+            this.btn_color.Tag = "2";
+            this.btn_color.Text = "Colors";
+            this.btn_color.Click += new System.EventHandler(this.btn_color_Click);
+            // 
+            // btn_job
+            // 
+            this.btn_job.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(169)))), ((int)(((byte)(146)))));
+            this.btn_job.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(169)))), ((int)(((byte)(146)))));
+            this.btn_job.BorderThickness = 2;
+            this.btn_job.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btn_job.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btn_job.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btn_job.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btn_job.FillColor = System.Drawing.Color.Transparent;
+            this.btn_job.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_job.ForeColor = System.Drawing.Color.Black;
+            this.btn_job.Location = new System.Drawing.Point(12, 77);
+            this.btn_job.Name = "btn_job";
+            this.btn_job.Size = new System.Drawing.Size(180, 45);
+            this.btn_job.TabIndex = 46;
+            this.btn_job.Tag = "1";
+            this.btn_job.Text = "Jobs";
+            this.btn_job.Click += new System.EventHandler(this.btn_job_Click);
+            // 
+            // Column1
+            // 
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.NullValue = ((object)(resources.GetObject("dataGridViewCellStyle3.NullValue")));
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.White;
+            this.Column1.DefaultCellStyle = dataGridViewCellStyle3;
+            this.Column1.HeaderText = "Image";
+            this.Column1.ImageLayout = System.Windows.Forms.DataGridViewImageCellLayout.Stretch;
+            this.Column1.Name = "Column1";
+            this.Column1.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column1.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Text";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Delete";
+            this.Column3.Name = "Column3";
+            // 
             // Dictionary
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = global::lab3.Properties.Resources._166504_Papel_de_Parede_Mario_Cenario_1600x1200;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(213)))), ((int)(((byte)(230)))), ((int)(((byte)(211)))));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1127, 673);
+            this.Controls.Add(this.btn_fruit);
+            this.Controls.Add(this.btn_animal);
+            this.Controls.Add(this.btn_color);
+            this.Controls.Add(this.btn_job);
+            this.Controls.Add(this.guna2Button3);
+            this.Controls.Add(this.guna2DataGridView1);
             this.Controls.Add(this.guna2CirclePictureBox1);
             this.Controls.Add(this.guna2CirclePictureBox6);
             this.Name = "Dictionary";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Dictionary";
             this.Load += new System.EventHandler(this.Dictionary_Load);
             ((System.ComponentModel.ISupportInitialize)(this.guna2CirclePictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.guna2CirclePictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2DataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -85,5 +284,14 @@
 
         private Guna.UI2.WinForms.Guna2CirclePictureBox guna2CirclePictureBox1;
         private Guna.UI2.WinForms.Guna2CirclePictureBox guna2CirclePictureBox6;
+        private Guna.UI2.WinForms.Guna2DataGridView guna2DataGridView1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button3;
+        private Guna.UI2.WinForms.Guna2Button btn_fruit;
+        private Guna.UI2.WinForms.Guna2Button btn_animal;
+        private Guna.UI2.WinForms.Guna2Button btn_color;
+        private Guna.UI2.WinForms.Guna2Button btn_job;
+        private System.Windows.Forms.DataGridViewImageColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
     }
 }
--- lab3/Dictionary.cs
+++ lab3/Dictionary.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Office.Interop.Excel;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -6,12 +7,24 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel.Application;
 using System.Windows.Forms;
 
 namespace lab3
 {
     public partial class Dictionary : Form
     {
+        Image image;
+        String tenImage,text;
+        String url = "G:\\Oanhhh\\c#\\image\\lab3\\Jobs";
+        string filePathExcel = "G:\\Oanhhh\\c#\\lab3\\lab3\\DicData.xlsx";
+        String topic = "Jobs";
+
+        Excel excel = new Excel();
+        Workbook wb;
+        Worksheet ws;
+        Range range;
+
         public Dictionary()
         {
             InitializeComponent();
@@ -19,6 +32,8 @@ namespace lab3
 
         private void guna2CirclePictureBox1_Click(object sender, EventArgs e)
         {
+            excel.Quit();
+
             this.Hide();
             Form1 form1 = new Form1();
             form1.ShowDialog();
@@ -39,6 +54,7 @@ namespace lab3
 
         private void Dictionary_Load(object sender, EventArgs e)
         {
+            loadData();
             if (Program.isPlay == true)
             {
                 Program.moNhac(guna2CirclePictureBox6);
@@ -49,5 +65,95 @@ namespace lab3
                 guna2CirclePictureBox6.Image = Image.FromFile("G:\\Oanhhh\\c#\\lab3\\lab3\\Resources\\volume-mute.png");
             }
         }
+
+        private void btn_job_Click(object sender, EventArgs e)
+        {
+            btn_job.BackColor = Color.FromArgb(71, 169, 146);
+            btn_animal.BackColor = Color.Transparent;
+            btn_fruit.BackColor = Color.Transparent;
+            btn_color.BackColor = Color.Transparent;
+            url = "G:\\Oanhhh\\c#\\image\\lab3\\Jobs";
+            topic = "Jobs";
+            loadData();
+        }
+
+        private void btn_color_Click(object sender, EventArgs e)
+        {
+            btn_color.BackColor = Color.FromArgb(71, 169, 146);
+            btn_animal.BackColor = Color.Transparent;
+            btn_fruit.BackColor = Color.Transparent;
+            btn_job.BackColor = Color.Transparent;
+            url = "G:\\Oanhhh\\c#\\image\\lab3\\color";
+            topic = "color";
+            loadData();
+        }
+
+        private void btn_animal_Click(object sender, EventArgs e)
+        {
+            btn_animal.BackColor = Color.FromArgb(71, 169, 146);
+            btn_job.BackColor = Color.Transparent;
+            btn_fruit.BackColor = Color.Transparent;
+            btn_color.BackColor = Color.Transparent;
+            url = "G:\\Oanhhh\\c#\\image\\lab3\\animal";
+            topic = "animal";
+            loadData();
+
+        }
+
+        private void btn_fruit_Click(object sender, EventArgs e)
+        {
+            btn_fruit.BackColor = Color.FromArgb(71, 169, 146);
+            btn_animal.BackColor = Color.Transparent;
+            btn_job.BackColor = Color.Transparent;
+            btn_color.BackColor = Color.Transparent;
+            url = "G:\\Oanhhh\\c#\\image\\lab3\\fruit";
+            topic = "fruit";
+            loadData();
+
+        }
+
+        private void loadData()
+        {
+            wb = excel.Workbooks.Open(filePathExcel);
+            ws = wb.Worksheets[topic];
+            range = ws.UsedRange;
+
+            for (int row = 1; row <= range.Rows.Count; ++row)//đọc row hiện có trong Excel
+            {
+                guna2DataGridView1.Rows.Add(new Bitmap(range.Cells[row, 1].Text), range.Cells[row, 2].Text);
+                //guna2DataGridView1.Sort(guna2DataGridView1.Columns[2], ListSortDirection.Ascending);
+            }
+            wb.Close();
+
+
+        }
+
+        private void guna2Button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.InitialDirectory = url;
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                tenImage = fileDialog.FileName;
+                String a = tenImage.Substring(tenImage.LastIndexOf("\\")+1);
+                text = a.Substring(0, a.LastIndexOf("."));
+
+                wb = excel.Workbooks.Open(filePathExcel);
+                ws = wb.Worksheets[topic];
+                range = ws.UsedRange;
+
+                int id = ws.UsedRange.Rows.Count + 1;
+                Range cells = ws.Range[$"A{id}:B{id}"];
+                string[] things = { tenImage,text };
+                cells.set_Value(XlRangeValueDataType.xlRangeValueDefault, things);
+
+                wb.Save();
+                wb.Close();
+                //excel.Quit();
+
+                guna2DataGridView1.Rows.Add(new Bitmap(tenImage), text);
+            }
+        }
+
     }
 }
